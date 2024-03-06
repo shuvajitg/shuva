a316836b4791644afbdccecf48e22101085aabd5
Shuvajit <shuvajitgayen57@gmail.come>
2024-03-06 22:28:31 +0530

    about section test

--- src/components/pageNav/nav.tsx
+++ src/components/pageNav/nav.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import React, { useState } from "react";
-
+import { BsMoonStarsFill } from "react-icons/bs";
 import { FiMenu } from "react-icons/fi";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { IoCloseOutline } from "react-icons/io5";
@@ -33,18 +33,19 @@ export default function Navbar() {
 
   return (
     <main>
-      <nav className="flex justify-between px-4 items-center py-6  ">
+      <nav className="flex justify-between p-6 items-center py-6 bg-slate-300 mt-1 mb-1 rounded-full">
         <div className="flex items-center gap-8">
           <section className="flex items-center gap-4">
             {/* logo */}
             <Link
               href={"/"}
-              className="text-4xl font-semibold uppercase font-mono"
+              className="sm:text-2xl lg:text-4xl text-xl font-semibold uppercase font-mono"
             >
               <span className="text-orange-600">shuva</span>jit
             </Link>
           </section>
         </div>
+        <BsMoonStarsFill className="sm:text-2xl lg:text-4xl text-xl font-semibold uppercase font-mono cursor-pointer text-slate-50" />
       </nav>
       <hr className=" " />
     </main>
